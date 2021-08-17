498fb413629f44ab5c51244608efcdd52535b39c
Kanthi Yerram <kyerram@cisco.com>
2021-08-17 18:46:33 +0530

    unittest cases

---
+++ gitreporttest.py
@@ -0,0 +1,24 @@
+import unittest
+import gitreport
+import requests
+import responses
+
+
+class Mytest(unittest.TestCase):
+   def test_user(self):
+       actual = gitreport.get_user_details(headers = {'Authorization': 'token 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'})
+       expected = {'login': 'kiran007', 'name': 'Kiran Reddy', 'email': 'kiran.yerram139@gmail.com'}
+       self.assertEqual(actual, expected)
+
+   def test_getlanguages(self):
+       actual = gitreport.get_all_languages(headers={'Authorization': 'token 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'}, repo="myrepo", org="githubreports")
+       expected = ["Java", "Python"]
+       self.assertEqual(actual, expected)
+
+   def test_generatecsv(self):
+       actual = gitreport.generate_csv_report({'login': 'kiran007', 'name': 'Kiran Reddy',\
+                                            'email': 'kiran.yerram139@gmail.com', \
+                                            "repos":"python_project,myrepo", "languages":"java,python,go"})
+       expected = self.assertis
+       self.assertEqual(actual, expected)
+
